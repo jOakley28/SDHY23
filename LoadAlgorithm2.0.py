56d4a37a64270fcfea80e3aafc04eb9d512b2c7d
jhankland <jhankland19@georgefox.edu>
2023-02-02 15:58:24 -0800

    debugged code which works up through mts plot. needs refinment on mts ration calcs

--- LoadAlgorithm2.0.py
+++ LoadAlgorithm2.0.py
@@ -27,61 +27,103 @@ warnings.filterwarnings('ignore')           #hide warnings
 
 
 def main():
-    pass
+    #handle raw ADC data
+    rawAdcData = pd.DataFrame()
+    print('Getting arduino data...')
+    rawAdcData, rawAdcFreq = import_arduino_data(arduinoDataFile,ardSepBy)    #import arduino data
+    print(f'{len(rawAdcData[0])} rows\n')  #prints total number of rows in datafile
+
+    correctedAdcData = detect_change_in_load(rawAdcData)    #look for change in ADC and write new data accordingly
+    correctedAdcData[1] = convert_adc_to_kg(correctedAdcData[1])   #TODO check if legal
+    #ADC DATA SHOULD NOW BE IN KG
 
 
-#TODO Import data
+    #handle mts data
+    mtsData = pd.DataFrame()
+    print('Gettting mts data...')
+    mtsData, mtsFreq = import_mts_data(mtsDataFile,mtsSepBy,mtsTimeShift) #imports mts data
+    print(f'{len(mtsData[0])} rows\n')  #prints total number of rows in datafile
+    mtsData[1] = correct_mts_data(mtsData[1]) #corrects mts (-N to +kg) (area ratio)
+
+
+    #calculate percent error
+    rawAdcDataCopy = rawAdcData.copy()
+    correctedAdcDataCopy = correctedAdcData.copy()
+    mtsDataCopy = mtsData.copy()
+
+    print('Estimating error...')
+    rAdcMtsError, rAvgError, rEbtw = errorCalculation(rawAdcData, mtsData)  #raw ADC vs MTS precent error 
+    cAdcMtsError, cAvgError, cEbtw = errorCalculation(correctedAdcData, mtsData)  #corrected ADC vs MTS precent error 
+
+
+    #create file names
+    print('Generating file names...\n')
+    rawMts_str, correctedMts_str, rawcorrectedMts_str = file_names(saveLocation)
+
+    #plot data
+    print('Plotting data...')
+    plot_two(rawAdcDataCopy, mtsDataCopy, rawAdcFreq, rAvgError, rAdcMtsError, rEbtw, rawMts_str)   #plot raw ADC and MTS 
+    plot_two(correctedAdcDataCopy, mtsDataCopy, rawAdcFreq, cAvgError, cAdcMtsError, cEbtw, correctedMts_str)   #plot corrected ADC and MTS 
+    #plot_three() #TODO
+    print('Plotting complete')
+
+
+#Import data
 def import_arduino_data(arduinoDataFile,ardSepBy):  #get arduino data from CSV file
     #put the data into a pandas dataframe
-    arduino_data_df = pd.read_csv(arduinoDataFile, sep=ardSepBy, header=1, low_memory=False)
-    print('Number of rows: ', len(arduino_data_df[0]))  #prints total number of rows in datafile
+    arduino_data_df = pd.read_csv(arduinoDataFile, sep=ardSepBy, header=None, low_memory=False)
 
     arduino_data_df[0] = arduino_data_df[0].apply(lambda x: x/3600)   #converts column 0 from seconds to hours
 
-    arduino_data_dt = (arduino_data_df.iloc[4,0] - arduino_data_df.iloc[0,0])/4    #determine dt based on change in time of the first four time steps
-    arduino_data_freq = int(1/arduino_data_dt)
+    arduino_data_dt = (arduino_data_df.iloc[4,0] - arduino_data_df.iloc[0,0])/5 #determine dt based on change in time of the first four time steps
+    arduino_data_freq = int(1/(3600*arduino_data_dt))
 
     #for testing
-    print(f'Arduino sampeling frequency {arduino_data_freq}hz')   #prints the sampling frequency in hz
-    input(arduino_data_df[:2]) #prints first 2 rows of arduino data
+    #print(f'Arduino sampeling frequency {arduino_data_freq}hz')   #prints the sampling frequency in hz
+    #input(arduino_data_df[:2]) #prints first 2 rows of arduino data
+    return arduino_data_df, arduino_data_freq #time (hours) & ADC (kg), arduino sample frequency
 
-    return arduino_data_df[2:], arduino_data_freq #time (hours) & ADC (kg), arduino sample frequency
-
-def import_mts_data(mtsDataFile,mtsSepBy,mtsTimeShift,areaRatio):  #get mts data from CSV file
+def import_mts_data(mtsDataFile,mtsSepBy,mtsTimeShift):  #get mts data from CSV file
     mts_data_df = pd.read_csv(mtsDataFile, sep=mtsSepBy, header=None, low_memory=False)
-
-    mts_data_dt = (mts_data_df.iloc[4,0] - mts_data_df.iloc[0,0])/4    #determine dt based on change in time of the first four time steps
-    mts_data_freq = int(1/mts_data_df)
+    
+    mts_data_dt = (mts_data_df.iloc[4,0] - mts_data_df.iloc[0,0])/5    #determine dt based on change in time of the first four time steps
+    mts_data_freq = int(1/mts_data_dt)
 
     #correct time
-    MTS_df[0] = MTS_df[0].apply(lambda x: x + mtsTimeShift)   #shift data right or left to account for inconsistant start times between devices - SECONDS!!
-    MTS_df[0] = MTS_df[0].apply(lambda x: ((x/60)/60))  #converts column 0 from seconds to hours
-
-    nToKg = -1/9.806 #downward force (N) (in the negative direction) to load (kg)
-    convert = nToKg/areaRatio #conversion coefficient 
-    MTS_df[1] = MTS_df[1].apply(lambda x: (x*convert)) #converts from applied mts load to local load on the sensor
-
+    mts_data_df[0] = mts_data_df[0].apply(lambda x: x + mtsTimeShift)   #shift data right or left to account for inconsistant start times between devices - SECONDS!!
+    mts_data_df[0] = mts_data_df[0].apply(lambda x: ((x/60)/60))  #converts column 0 from seconds to hours
+    
     #for testing
-    print(f'MTS sampeling frequency: {mts_data_freq}hz')
-    input(mts_df[:2]) #prints first 2 row of mts data
+    #print(f'MTS sampeling frequency: {mts_data_freq}hz')
+    #input(mts_data_df[:2]) #prints first 2 row of mts data
 
-    return MTS_df[1:], mts_data_freq #time (hours) & mts applied laoding (kg), mts sample frequency
+    return mts_data_df[0:], mts_data_freq #time (hours) & mts applied laoding (kg), mts sample frequency
+
+def correct_mts_data(rawMtsData):
+    rawMtsData_df = rawMtsData
+    blockArea = blockLength*blockWidth    #difined by user (in^2)
+    sensorArea = 0.785852   #from data sheet (in^2)
+    areaRatio = sensorArea/blockArea
+    nToKg = -1/9.806    #downward force (N) (in the negative direction) to load (kg)
+    corFactor = nToKg/areaRatio
+    correctedMtsData = rawMtsData.apply(lambda x: x*corFactor)
+    return correctedMtsData
 
 
 #TODO Determine if there was a change in loading 
-def detect_change_in_load():    #determine if there was a change in loading 
-    pass
+def detect_change_in_load(data):    #determine if there was a change in loading 
+    return data
 
 def take_first_derivative(time, data):  #takes the first derivative of a dataset 
     dataFirstDir = [0]   #creates array ofset by 1 to account for 1st dir offset 
-    for i in range(len(data)-1) #takes the difrence between each two data points and divides by the change in time
+    for i in range(len(data)-1): #takes the difrence between each two data points and divides by the change in time
         ithDerivative = (data[i+1] - data[i])/(time[i+1]-time[i])
         dataFirstDir = dataFirstDir.append(ithDerivative) #appends the derivative array with the next ith derivative 
     return dataFirstDir
 
 def take_second_derivative(time, dataFirstDir):  #takes the second derivative of a dataset
     dataSecondDir = [0,0]   #creates array ofset by 2 to account for 2nd dir offset 
-    for i in range(len(dataFirstDir)-1) #takes the difrence between each two data points and divides by the change in time
+    for i in range(len(dataFirstDir)-1): #takes the difrence between each two data points and divides by the change in time
         ithDerivative = (dataFirstDir[i+1] - dataFirstDir[i])/(time[i+1]-time[i])
         dataSecondDir = dataSecondDir.append(ithDerivative) #appends the derivative array with the next ith derivative 
     return dataSecondDir
@@ -89,21 +131,36 @@ def take_second_derivative(time, dataFirstDir):  #takes the second derivative of
 def write_new_load():   #based on detect_chagne_in_load, write new loading 
     pass
 
-def convert_adc_to_kg(adcData):    #convert adc to kg (using change in ADC)
-    return adcData
+def convert_adc_to_kg(adcData): #TODO convert adc to kg (using change in ADC)
+    #adcDataKg = pd.DataFrame()
+    adcDataKg = adcData
+
+    [m,b] = [0.007726,-77.5564] #circut B profile
+    adcDataKg = adcDataKg.apply(lambda x: m*x + b)
+    return adcDataKg
 
 
 #Error approximation 
-def errorInData(sampleA, sampleB):  #determine the error (both instantenous and average)
+def errorCalculation(sampleA,sampleB):
+    resized_df = stretch_data(sampleA[0],sampleA[1],sampleB[0],sampleB[1])
+    ebtw = pd.DataFrame()   #bounds for fill between in plots
+    ebtw[0] = resized_df[0]
+    ebtw[1] = resized_df[1]
+    ebtw[2] = resized_df[3]
+
+    errorC, avgError = errorBetweenCurves(resized_df[1], resized_df[3])
+    return errorC, avgError, ebtw
+
+def errorBetweenCurves(sampleA, sampleB):  #determine the error (both instantenous and average)\
     c = []
     errorC = []    
     for i in range(len(sampleA)):
         c.append(abs(sampleA[i] - sampleB[i]))
         errorC.append(100*c[i]/sampleB[i])
-    avgError = int(abs(100*np.mean(error)))/100
-    return avgError, errorC  #average error, error at each data point 
+    avgError = int(abs(100*np.mean(errorC)))/100
+    return errorC, avgError #average error, error at each data point 
 
-def stretch_data(sampleAx,sampleAy,sampleBx,sampleBy):    #makes both arrays the same length (used for error calculation)
+def stretch_data(sampleAx: np.array,sampleAy: np.array,sampleBx: np.array,sampleBy: np.array):    #makes both arrays the same length (used for error calculation)
     #determine the size of the two samples
     minM = np.min(sampleBx)
     minA = np.min(sampleAx)
@@ -166,32 +223,104 @@ def stretch_data(sampleAx,sampleAy,sampleBx,sampleBy):    #makes both arrays the
 
 
     holder_df = pd.DataFrame() 
-    holder_df[0] = np.arange(0,new_len,1)
-    holder_df[1] = interp1d(sampleAresize_df[0], new_len)
-    holder_df[2] = interp1d(sampleAresize_df[1], new_len)
-    holder_df[3] = interp1d(sampleBresize_df[0], new_len)
-    holder_df[4] = interp1d(sampleBresize_df[1], new_len)
+    holder_df[0] = lin_interpolate(sampleAresize_df[0], new_len)
+    holder_df[1] = lin_interpolate(sampleAresize_df[1], new_len)
+    holder_df[2] = lin_interpolate(sampleBresize_df[0], new_len)
+    holder_df[3] = lin_interpolate(sampleBresize_df[1], new_len)
 
-    return holder_df[1:]    #Ax, Ay, Bx, By
+    return holder_df    #Ax, Ay, Bx, By
 
-def interpolate(array: np.ndarray, new_len: int) -> np.ndarray: #used in stretch data
+def lin_interpolate(array: np.ndarray, new_len: int) -> np.ndarray: #used in stretch data
     la = len(array)
     return np.interp(np.linspace(0, la - 1, num=new_len), np.arange(la), array)
 
 
 #TODO Output
-def plot_r():   #plot only the raw data
+def plot_two(ard, mts, ard_freq, avgArdError, ardError, errorBtw, address):   #2 col df where 0: time, 1: data
+    plt.plot(mts[0],mts[1],color='r',linewidth=1.0,alpha=0.7,label=f'Applied MTS')    #MTS load
+    plt.plot(ard[0],ard[1],color='c',linewidth=0.8,label=f'Raw ({int(avgArdError*10)/10}%)')      #raw load
+
+    plt.minorticks_on()
+    plt.grid(visible=None, which='major', linestyle='-', linewidth='1', axis='both')
+    plt.grid(visible=None, which='minor', linestyle='--', linewidth='0.7', axis='y')
+
+    plt.xlim(ard.iloc[0,0],ard.iloc[len(ard)-1,0])
+    plt.autoscale(False, axis="y")
+    plt.ylim(0,110)    
+    plt.yticks(np.arange(0,111, step=10))
+    plt.legend()
+
+    plt.title(f'{address[0]}\n{testInfo}') 
+    plt.title(f'@{int(ard_freq)}hz - latency* 0s',loc='right',color='g')
+    #plt.title(f'Circuit {circuitType}',loc='left') #TODO add both circut profiles if nessacary 
+    plt.xlabel("Time (hrs)")
+    plt.ylabel("Load (kg)")
+
+    plt.fill_between(errorBtw[0], errorBtw[1], errorBtw[2], color='gray', alpha=0.2, label='error')
+    plt.savefig(address[1])
+    plt.cla() 
+
+def plot_three(): #plot both raw and filtered data
     pass
 
-def plot_r_and_f(): #plot both raw and filtered data
-    pass
-
-def open_output_in_folder(loc):    #opens the folder where data was saved 
-    pf = pathlib.Path(f'{loc}')
+def open_output_in_folder(saveLocation):    #opens the folder where data was saved 
+    pf = pathlib.Path(f'{saveLocation}')
     runLf = f'/{pathlib.Path(*pf.parts[3:])}'   #trims address to only include what is relevent for windows address 
     runWf = pathlib.PureWindowsPath(runLf)      #converts to windows path 
     proc = subprocess.Popen(['explorer.exe', runWf])
 
+def file_names(saveLocation):
+    #plot with raw with mts data
+    rawMts = [f'raw with mts',' ']
+    rawMts[1] = f'{saveLocation}/{rawMts[0]}.png'
+
+    #plot with fixed and mts data
+    fixedMts = [f'corrected with mts',' ']
+    fixedMts[1] = f'{saveLocation}/{fixedMts[0]}.png'
+
+    #plot with raw, fixed, and mts data
+    rawFixedMts = [f'raw and corrected with mts',' ']
+    rawFixedMts[1] = f'{saveLocation}/{rawFixedMts[0]}.png'
+
+    return rawMts, fixedMts, rawFixedMts
+
 
 if __name__ == "__main__":  
+    #TODO add argument parser
+
+    #arduino
+    arduinoDataFile = 'log6/LOG_6.txt'
+    ardSepBy = '\t'
+
+    #MTS
+    mtsDataFile = 'log6/hy_test1_glue6.txt'
+    mtsSepBy = '\t'
+    mtsTimeShift = 0.00 #in seconds
+    blockLength = 2.5 #in inches
+    blockWidth = 2.5  #in inches
+
+    #saving
+    testInfo = 'building algorithm'
+    saveLocation  = 'log6/TEST DIR (log)'
+
+    #checks if arduino datafile exists
+    if not os.path.isfile(arduinoDataFile):
+        print("Data file not found")
+        exit(1)
+
+    #checks if MTS datafile exists
+    if not os.path.isfile(mtsDataFile):
+        print("MTS file not found")
+        exit(1)       
+
+    #creates save folder if necessary 
+    if not os.path.exists(saveLocation):
+        os.makedirs(saveLocation)
+        print(f'created \"{saveLocation}\" dir\n')
+
+    #run main loop
     main()
+
+    if True == False:
+        #open folder where plots are saved
+        open_output_in_folder(saveLocation)
